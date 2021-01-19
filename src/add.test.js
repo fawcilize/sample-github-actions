056e4a14a02addf39084cef6f7cea4d1fe2f4bae
Kevin Fawcett <kevin.fawcett@jupiterone.com>
2021-01-18 21:28:06 -0500

    Adding additional test for the add function

--- src/add.test.js
+++ src/add.test.js
@@ -7,4 +7,9 @@ describe("add", () => {
     const result = add(1, 2);
     expect(result).toEqual(3);
   });
+
+  test("1 + -1 = 0", () => {
+    const result = add(1, -1);
+    expect(result).toEqual(0);
+  });
 });
