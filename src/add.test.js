a6b801cae16e5d5238a397ae0c85ab30abfc4933
Kevin Fawcett <kevin.fawcett@jupiterone.com>
2021-01-19 20:43:48 -0500

    Adding additional test case

--- src/add.test.js
+++ src/add.test.js
@@ -3,8 +3,13 @@ function add(a, b) {
 }
 
 describe("add", () => {
-  test("1 + 2 = 3", () => {
+  test("adds numbers", () => {
     const result = add(1, 2);
     expect(result).toEqual(3);
+  });
+
+  test("works with negative numbers", () => {
+    const result = add(1, -1);
+    expect(result).toEqual(0);
   });
 });
